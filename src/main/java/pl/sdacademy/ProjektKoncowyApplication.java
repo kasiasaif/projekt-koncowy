90a5c668f49cbec6c9a4b0411d7d009bdd79d248
kasiasaif <kasia.saif@gmail.com>
2021-08-19 10:24:52 +0100

    project init

---
+++ src/main/java/pl/sdacademy/ProjektKoncowyApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektKoncowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektKoncowyApplication.class, args);
+    }
+
+}
