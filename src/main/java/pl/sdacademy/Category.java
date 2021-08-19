8029d48e231e8c219513ad7fa0cfe663810ae49e
Joanna <you@example.com>
2021-08-19 13:31:48 +0200

    Category encja

---
+++ src/main/java/pl/sdacademy/Category.java
@@ -0,0 +1,31 @@
+package pl.sdacademy;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public Category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                '}';
+    }
+}
